e27caa78928c9552853fbb9821b8726959d624ec
JongWoo Ha <jongwooha98@gmail.com>
2023-08-20 18:12:48 +0900

    wip: Sample code for Emotion usage

---
+++ src/pages/Profile/AddPetButton.tsx
@@ -0,0 +1,23 @@
+import Button  from "../../components/Button/index.tsx"
+
+ function AddPetButton() {
+
+    const handeClick1 =() => {
+        console.log('애완견 추가')
+    }
+    const handeClick2 =() => {
+        console.log('애완견 삭제')
+    }
+    const handeClick3 =() => {
+        console.log('애완견 입양')
+    }
+    return (
+        <>
+            <Button onClick={handeClick1}/>
+            <Button onClick={handeClick2}/>
+            <Button onClick={handeClick3}/>
+        </>
+    )
+}
+
+export default AddPetButton
