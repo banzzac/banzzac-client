b9acf192fef75d8141e70e6754b8d0f6aa170590
Ryu_woong <jin02014@naver.com>
2023-09-19 20:09:08 +0900

    Design: CSS 리셋 적용했습니다.
    App.tsx에 Global 컴포넌트가 Reset.css 적용합니다.
    h4 태그, p 태그 등이 기본 margin을 가지고 있고, 변경해보려 했으나 적용되지 않아서 reset.css를 적용시켰습니다.
    또한 기본 폰트를 SUIT으로 적용시켰습니다.
    
    Fixes : 그러나, 일부분에서는 SUIT 이   적용되지 않는 것 같습니다. 이 부분은 어떻게 해결할 지 의논이 필요합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Global } from "@emotion/react";
+
+import reset from "./reset";
 import { Navigator } from "./routes/Navigator";
 
 function App() {
   return (
     <div>
+      <Global styles={reset} />
       <Navigator />
     </div>
   );
