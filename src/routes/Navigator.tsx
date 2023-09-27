c93d4851e3610cf1abb4a97d28b1a5c201a52b37
Ryu_woong <jin02014@naver.com>
2023-09-27 19:12:00 +0900

    Design : Navigator 와 ChatRoom 디자인 수정했습니다.
    Navigator
    Fix :
    1) Nav가 fixed라서 화면 크기 변동에 따라 위치가 변합니다. %로 해도 답이 없음 이거 해결법 없죠?
    2) 3번째 Link Absolute 위치가 화면 크기에 따라 변합니다. 그리고 width,height 크기를 고정해 놔서 이미지 크기도 커져있습니다. 수정이 필요합니다. Help me!
    
    Chat
    fix: 화면의 크기가 작아질 경우 absolute 영향으로 영역을 침범합니다. 화면 크기 별로 수정 해야하는데 우리 이것도 해요?
    
    Comment : 화면 IPhone XR 기준으로 작성했습니다. 확인 해주세요.

--- src/routes/Navigator.tsx
+++ src/routes/Navigator.tsx
@@ -1,16 +1,70 @@
+import styled from "@emotion/styled";
 import { Link, Outlet } from "react-router-dom";
+
+// Import Icons`
+import chatIcon from "./../img/Icons/Chat-Icon.png";
+import feedIcon from "./../img/Icons/Feed-Icon.png";
+import firendIcon from "./../img/Icons/Friend-Icon.png";
+import profileIcon from "./../img/Icons/Profile-Icon.png";
+import searchIcon from "./../img/Icons/Search-Icon.png";
+
+// Styled Zone
+const Nav = styled.nav`
+  width: 360px;
+  position: fixed;
+  z-index: 5;
+  display: flex;
+  justify-content: space-between;
+  align-item: center;
+  bottom: 15px;
+  left: 17%;
+
+  padding: 13px 48px;
+  box-sizing: border-box;
+  border-radius: 67px;
+  background: #fff;
+  box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.3);
+`;
+const FooterImg = styled.img``;
+const SearchDiv = styled.div`
+  position: relative;
+`;
+const SearchImg = styled.img`
+  position: absolute;
+  left: -18px;
+  top: -35px;
+  width: 50px;
+  height: 50px;
+  font-size: 15px;
+  background-color: #a463ff;
+  filter: drop-shadow(0px 0px 15px rgba(0, 0, 0, 0.3));
+  border-radius: 25px;
+  object-fit: cover;
+`;
 
 export const Navigator = () => {
   return (
     <div>
       <Outlet />
-      <nav>
-        <Link to={"profile"}>link</Link>
-        <Link to={"chat"}>chat</Link>
-        <Link to={"friends"}>friends</Link>
-        <Link to={"feed"}>feed</Link>
-        <Link to={"search"}>search</Link>
-      </nav>
+      <Nav>
+        <Link to={"profile"}>
+          <FooterImg src={profileIcon} alt="Profile" />
+        </Link>
+        <Link to={"chat"}>
+          <FooterImg src={chatIcon} alt="Chat" />
+        </Link>
+        <Link to={"search"}>
+          <SearchDiv>
+            <SearchImg src={searchIcon} alt="Search" />
+          </SearchDiv>
+        </Link>
+        <Link to={"friends"}>
+          <FooterImg src={firendIcon} alt="Friends" />
+        </Link>
+        <Link to={"feed"}>
+          <FooterImg src={feedIcon} alt="Feed" />
+        </Link>
+      </Nav>
     </div>
   );
 };
